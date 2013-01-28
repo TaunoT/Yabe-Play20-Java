38946d93bf3e0d414847551164e7ac28e387c2af
Tauno Tammemäe <Tauno.Tammemae@gmail.com>
2013-01-28 21:11:19 +0200

    Just some formatting

--- app/models/Post.java
+++ app/models/Post.java
@@ -21,58 +21,57 @@ import play.db.ebean.Model;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name="posts")
+@Table(name = "posts")
 public class Post extends Model {
-	
-	@Id
-	public Long id;
 
-	public String title;
-	public Date postedAt;
+  @Id
+  public Long id;
 
-	@Lob
+  public String title;
+
+  public Date postedAt;
+
+  @Lob
   @Basic(fetch = FetchType.EAGER)
-	public String content;
-	
-	@ManyToOne
-	public User author;
-	
-	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
-	public List<Comment> comments = new ArrayList<Comment>();
-  
+  public String content;
+
+  @ManyToOne
+  public User author;
+
+  @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+  public List<Comment> comments = new ArrayList<Comment>();
+
   @ManyToMany(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
   public Set<Tag> tags = new TreeSet<Tag>();
 
-  public static Model.Finder<Long, Post> find = new Model.Finder(Long.class, Post.class);
+  public static Model.Finder<Long, Post> find = new Model.Finder(Long.class,Post.class);
 
-	public Post addComment(String author_fullname, String author_email, String content) {
+  public Post addComment(String author_fullname, String author_email, String content) {
     User commenter = User.findByEmail(author_email);
     Comment newComment;
-    if(commenter != null){
+    if (commenter != null) {
       newComment = new Comment(this, commenter, content);
-    }
-    else {
+    } else {
       commenter = new User(author_email, "", author_fullname);
       commenter.save();
       newComment = new Comment(this, commenter, content);
     }
-		newComment.save();
-		this.comments.add(newComment);
-		this.save();
-		return this;
-	}
+    newComment.save();
+    this.comments.add(newComment);
+    this.save();
+    return this;
+  }
 
   public Post(String title, User author, String content) {
-    //this.comments = new ArrayList<Comment>();
-    //this.tags = new TreeSet<Tag>();
+    // this.comments = new ArrayList<Comment>();
+    // this.tags = new TreeSet<Tag>();
     this.author = author;
     this.title = title;
     this.content = content;
     this.postedAt = new Date();
   }
-  
-  public static Post create(Post post, String email, String content)
-  {
+
+  public static Post create(Post post, String email, String content) {
     post.author = User.find.where().eq("email", email).findUnique();
     post.content = content;
     post.save();
@@ -80,33 +79,35 @@ public class Post extends Model {
   }
 
   public Post previous() {
-    Post result = Post.find.where().lt("postedAt", postedAt).orderBy("postedAt desc").setMaxRows(1).findUnique();
-    if(result != null)
+    Post result = Post.find.where().lt("postedAt", postedAt)
+        .orderBy("postedAt desc").setMaxRows(1).findUnique();
+    if (result != null)
       return result;
     return null;
   }
-  
+
   public Post next() {
-    Post result = Post.find.where().gt("postedAt", postedAt).orderBy("postedAt asc").setMaxRows(1).findUnique();
-    if(result != null)
+    Post result = Post.find.where().gt("postedAt", postedAt)
+        .orderBy("postedAt asc").setMaxRows(1).findUnique();
+    if (result != null)
       return result;
     return null;
   }
-  
+
   public Post tagItWith(String name) {
     tags.add(Tag.findOrCreateByName(name));
     this.saveManyToManyAssociations("tags");
     return this;
   }
-    
+
   public static List<Post> findTaggedWith(String... tags) {
-    //String sql = "select distinct p from Post p join p.tags as t where t.name in (:tags) group by p.id, p.author, p.title, p.content, p.postedAt having count(t.id) = :size";
+    // String sql =
+    // "select distinct p from Post p join p.tags as t where t.name in (:tags) group by p.id, p.author, p.title, p.content, p.postedAt having count(t.id) = :size";
     return Post.find.where().in("tags.name", tags).findList();
     /*
-    RawSql rawsql = RawSqlBuilder.parse(sql).create();
-    com.avaje.ebean.Query<Post> sqlquery = Ebean.find(Post.class);
-    sqlquery.setRawSql(rawsql);
-    return sqlquery.findList();
-    */
+     * RawSql rawsql = RawSqlBuilder.parse(sql).create();
+     * com.avaje.ebean.Query<Post> sqlquery = Ebean.find(Post.class);
+     * sqlquery.setRawSql(rawsql); return sqlquery.findList();
+     */
   }
 }
