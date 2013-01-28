c5816edf3f6bf0fab6aaff949956e902ee05b9eb
Tauno Tammemäe <Tauno.Tammemae@gmail.com>
2013-01-28 20:58:12 +0200

    Just some formatting

--- app/models/User.java
+++ app/models/User.java
@@ -10,31 +10,32 @@ import play.db.ebean.Model;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User extends Model {
 
   @Id
-	public String email;
-	public String password;
-	public String fullname;
-	public boolean isAdmin;
+  public String email;
+  public String password;
+  public String fullname;
+  public boolean isAdmin;
 
-	public User(String email, String password, String fullname) {
-		this.email = email;
-		this.password = password;
-		this.fullname = fullname;
-	}
-		
-	public static Model.Finder<String,User> find = new Model.Finder(String.class, User.class);
+  public User(String email, String password, String fullname) {
+    this.email = email;
+    this.password = password;
+    this.fullname = fullname;
+  }
+
+  public static Model.Finder<String, User> find = new Model.Finder(String.class, User.class);
 
   public static List<User> findAll() {
     return find.all();
   }
-	
+
   public static User findByEmail(String email) {
     return find.where().eq("email", email).findUnique();
   }
-	public static User authenticate(String email, String password) {
-		return find.where().eq("email", email).eq("password", password).findUnique(); 
-	}
+
+  public static User authenticate(String email, String password) {
+    return find.where().eq("email", email).eq("password", password).findUnique();
+  }
 }
